8f0dfeb4dfc20c553697c6e24ba99f1f136c58f0
Andrey Serikov <AS.Korag@protonmail.com>
2021-02-25 09:33:42 +0300

    refactor: replace switch with for in

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,4 @@
-module.exports = getSeason = (date) => {
+module.exports = getSeason = date => {
   if (date === undefined) {
     return 'Unable to determine the time of year!'
   }
@@ -7,22 +7,18 @@ module.exports = getSeason = (date) => {
     throw Error
   }
 
+  const seasons = {
+    winter: [11, 0, 1],
+    spring: [2, 3, 4],
+    summer: [5, 6, 7],
+    autumn: [8, 9, 10],
+  }
+
   const month = date.getMonth()
 
-  switch (month) {
-    case 0:
-    case 1:
-    case 11:
-      return 'winter'
-    case 2:
-    case 3:
-    case 4:
-      return 'spring'
-    case 5:
-    case 6:
-    case 7:
-      return 'summer'
-    default:
-      return 'autumn'
+  for (let key in seasons) {
+    if (seasons[key].includes(month)) {
+      return key
+    }
   }
 }
