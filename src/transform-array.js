3da7c843a804f081a1ae8dbe115c17493aedd5fb
AS.Korag <as.korag@protonmail.com>
2021-02-27 01:42:37 +0300

    refactor: reduce transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,169 +3,70 @@ module.exports = transform = (array) => {
     throw new Error()
   }
 
-  const escapeSequences = [
-    '--discard-next',
-    '--discard-prev',
-    '--double-next',
-    '--double-prev',
-  ]
-
-  const dcp = '--discard-prev'
-  const dcn = '--discard-next'
-  const dbp = '--double-prev'
-  const dbn = '--double-next'
-
-  const discardRegExp = /--discard-(prev|next)/
-  const doubleRegExp = /--double-(prev|next)/
+  const controlSeqRegExp = /--(discard|double)-(prev|next)/
   const prevRegExp = /--(discard|double)-prev/
   const nextRegExp = /--(discard|double)-next/
 
-  let newArray = [...array]
+  let changedArray = array.map((item, index) => {
+    const prevItem = array[index - 1]
+    const nextItem = array[index + 1]
 
-  // empty array
-  if (newArray.length === 0) {
-    return newArray
-  }
-
-  //simple array
-  if (
-    !newArray.includes(dcp) &&
-    !newArray.includes(dcn) &&
-    !newArray.includes(dbp) &&
-    !newArray.includes(dbn)
-  ) {
-    return newArray
-  }
-
-  let na = newArray.map((item, index) => {
-    const prevItem = newArray[index - 1]
-    const nextItem = newArray[index + 1]
-
-    //1.1 - 1.3 - 1.5
+    //returned unchanged element
     if (
-      !escapeSequences.includes(prevItem) &&
-      (!escapeSequences.includes(nextItem) || nextRegExp.test(nextItem))
+      ((!controlSeqRegExp.test(prevItem) || prevRegExp.test(prevItem)) &&
+        (!controlSeqRegExp.test(nextItem) || nextRegExp.test(nextItem))) ||
+      (prevItem === '--double-next' && nextItem === '--discard-prev')
     ) {
       return item
     }
 
-    //3
-    if (prevItem === dcn) {
-      return {
-        value: item,
-        discard: true,
-        amount: 1,
-      }
-    }
-
-    //2.1 - 2.3 - 2.5
+    //returned deleted element
     if (
-      prevItem === dcp &&
-      (!escapeSequences.includes(nextItem) || nextRegExp.test(nextItem))
-    ) {
-      return item
-    }
-
-    //4.1 - 4.3 - 4.5
-    if (
-      prevItem === dbp &&
-      (!escapeSequences.includes(nextItem) || nextRegExp.test(nextItem))
-    ) {
-      return item
-    }
-
-    //1.4 - 2.4 - 4.4
-    if (
-      (!escapeSequences.includes(prevItem) || prevRegExp.test(prevItem)) &&
-      nextItem === dbp
-    ) {
-      return {
-        value: item,
-        discard: false,
-        amount: 2,
-      }
-    }
-
-    //1.2 - 2.2 - 4.2
-    if (
-      (!escapeSequences.includes(prevItem) || prevRegExp.test(prevItem)) &&
-      nextItem === dcp
+      prevItem === '--discard-next' ||
+      ((!controlSeqRegExp.test(prevItem) || prevRegExp.test(prevItem)) &&
+        nextItem === '--discard-prev')
     ) {
       return {
         value: item,
         discard: true,
-        amount: 1,
       }
     }
 
-    //5.1 - 5.3 - 5.5
+    //returned doubled element
     if (
-      prevItem === dbn &&
-      (!escapeSequences.includes(nextItem) || nextRegExp.test(nextItem))
+      ((!controlSeqRegExp.test(prevItem) || prevRegExp.test(prevItem)) &&
+        nextItem === '--double-prev') ||
+      (prevItem === '--double-next' &&
+        (!controlSeqRegExp.test(nextItem) || nextRegExp.test(nextItem)))
     ) {
       return {
         value: item,
-        discard: false,
         amount: 2,
       }
     }
 
-    //5.2
-    if (prevItem === dbn && nextItem === dcp) {
-      return item
-    }
-
-    //5.4
-    if (prevItem === dbn && nextItem === dbp) {
+    //returned tripled element
+    if (prevItem === '--double-next' && nextItem === '--double-prev') {
       return {
         value: item,
-        discard: false,
         amount: 3,
       }
     }
   })
 
-  const ab = []
+  const transformedArray = []
 
-  // for (let item of na) {
-  //   if (
-  //     !escapeSequences.includes(item) ||
-  //     (typeof item === 'object' && item.discard === false)
-  //   ) {
-  //     if (typeof item === 'object' && item.amount === 2) {
-  //       ab.push(item.value)
-  //       ab.push(item.value)
-  //     }
-
-  //     if (typeof item === 'object' && item.amount === 3) {
-  //       ab.push(item.value)
-  //       ab.push(item.value)
-  //       ab.push(item.value)
-  //     }
-
-  //     if (typeof item !== 'object') {
-  //       ab.push(item)
-  //     }
-  //   }
-  // }
-
-  for (let item of na) {
-    if (typeof item === 'object' && item.hasOwnProperty('discard')) {
-      if (item.discard === false) {
-        if (item.amount === 2) {
-          ab.push(item.value)
-          ab.push(item.value)
-        }
-        if (item.amount === 3) {
-          ab.push(item.value)
-          ab.push(item.value)
-          ab.push(item.value)
-        }
+  for (let item of changedArray) {
+    if (typeof item === 'object' && item.hasOwnProperty('value')) {
+      if (item.amount !== undefined) {
+        transformedArray.push(...Array(item.amount).fill(item.value))
       }
     } else {
-      ab.push(item)
+      if (!controlSeqRegExp.test(item)) {
+        transformedArray.push(item)
+      }
     }
   }
 
-  return ab.filter((item) => !escapeSequences.includes(item))
+  return transformedArray
 }
